651278018c5ccce4467b3e2e288148217a95afed
bikramsk <bikramkanyal@gmail.com>
2025-07-11 20:06:03 +0530

    update login

--- src/components/custom/Login.jsx
+++ src/components/custom/Login.jsx
@@ -561,7 +561,7 @@ const Login = () => {
     }
     
     // If no draft, go to homepage
-    navigate('/', { replace: true });
+    navigate('/my-account', { replace: true });
   };
 
   const handleSubmit = async (e) => {
