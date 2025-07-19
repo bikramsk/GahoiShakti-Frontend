00ad2321e62657db32b49e7cec54b52d379b4eb4
bikramsk <bikramkanyal@gmail.com>
2025-07-19 12:57:20 +0530

    updated 2025 sammelan banner link

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -158,7 +158,8 @@ const HeroSection = () => {
 
 {currentImageIndex === 0 ? (
   <a
-    href="https://docs.google.com/forms/d/e/1FAIpQLSfm9qnGFfnsv8lPHN8cJM3t52T4qXOXE7THz_F9CycXaewGdA/viewform?usp=header"
+    href="https://forms.gle/pUzLxYCfeFhWGtKe6"
+    // href="https://docs.google.com/forms/d/e/1FAIpQLSfm9qnGFfnsv8lPHN8cJM3t52T4qXOXE7THz_F9CycXaewGdA/viewform?usp=header"
     target="_blank"
     rel="noopener noreferrer"
   >
